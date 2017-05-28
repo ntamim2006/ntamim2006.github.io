0571a66904ba6f238b2b02732ab5558c45445eb4
Nadav - home <ntamim2006@gmail.com>
2017-05-28 22:29:37 +0300

    d

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function(){
 				//eventTitle: event.name,
 				//eventDetails: event.event_details,
 				event_start: event.date + " 00:00:00",
-				event_end: event.date + "24:00:00",
+				event_end: event.date + " 24:00:00",
 				//event_time: "תאריך החתונה: " + event.date ,
 				event_title: event.name,
 				event_place: event.place,
